1c8f23900efe87215406c1e44562c8962d726950
Sekgomotso_s <motsoshalang@gmail.com>
2020-03-16 12:04:50 +0200

    html static page

---
+++ src/functions.js
@@ -0,0 +1,45 @@
+// Save visitor into database
+const addNewVisitor = async(name, age, date, time, nameOfAssistant, comment) => {
+
+    try{
+    
+      query = await pool.query(
+        "INSERT INTO Visitors (visitor_name, visitors_age, date_of_visit, time_of_visit, assistant, comments) values ($1, $2, $3, $4, $5, $6)", 
+        [name, age, date, time, nameOfAssistant, comment]);
+  
+        return query.rows
+  
+    } catch(err) {
+      console.log(err)
+  
+    }
+  };
+
+// Return an array of visitor's names and ID
+const viewVisitors = async () => {
+
+    try {
+      query = pool.query(
+        "SELECT id, visitor_name FROM Visitors",
+        []);
+  
+        return query.rows
+  
+    } catch(err){
+       console.log(err)
+    }
+};
+
+// Delete a visitor
+const deleteVisitor = async () => {
+    try {
+      query = pool.query(
+        "DELETE FROM Visitors WHERE id = ${id}", []);
+  
+        return query.rows
+  
+    }catch(err){
+      console.log(err)
+  
+    }
+  };
