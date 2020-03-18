875d9b1501b8506ce97063a27d4040d0f9be51ad
Sekgomotso_s <motsoshalang@gmail.com>
2020-03-18 08:03:07 +0200

    single-app-page

--- src/server.js
+++ src/server.js
@@ -7,9 +7,42 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + "/ajax.html"))
+    res.sendFile(path.join(__dirname + "/ajax.html"));
 });
 
-app.listen(3009, (req, res) => {
+// add visitor
+app.post('/single-page-app', (req, res) => {
+
+    addNewVisitor(req.body.visitor_name, 
+      req.body.assistant, 
+      req.body.visitors_age, 
+      req.body.date_of_visit, 
+      req.body.time_of_visit, 
+      req.body.comments)
+      
+  res.sendFile(__dirname + '/ajax.html')
+  
+});
+
+// Save visitor into database
+const addNewVisitor = async(name, nameOfAssistant, age, date, time, comments) => {
+
+    try{
+
+        query = await pool.query(
+        "INSERT INTO Visitors (visitor_name, assistant, visitors_age, date_of_visit, time_of_visit, comments) values ($1, $2, $3, $4, $5, $6)", 
+        [name, nameOfAssistant, age, date, time, comments]);
+
+        return query.rows
+
+    } catch(err) {
+        console.log(err)
+
+    }
+};
+
+const server = app.listen(3009, (req, res) => {
     console.log("server listening on port 3009")
 })
+
+module.exports = server;
