46c81953bcd46c12d5ffbfa40871aa8b32378220
Sekgomotso_s <motsoshalang@gmail.com>
2020-05-25 16:04:49 +0200

    WIP

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const cors = required ('cors');
-const {addNewVisitor, viewVisitors, deleteVisitor} = require('./functions');
+const cors = require('cors');
+// const {addNewVisitor, viewVisitors, deleteVisitor} = require('./functions');
 
 // set up server
 const app = express();
@@ -17,34 +17,51 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/ajax.html"));
 });
 
-// add visitor
-app.post('/single-page-app', (req, res) => {
+// // add visitor
+// app.post('/single-page-app', (req, res) => {
 
-    addNewVisitor(req.body.visitor_name, 
-      req.body.assistant, 
-      req.body.visitors_age, 
-      req.body.date_of_visit, 
-      req.body.time_of_visit, 
-      req.body.comments)
+//     addNewVisitor(req.body.visitor_name, 
+//       req.body.assistant, 
+//       req.body.visitors_age, 
+//       req.body.date_of_visit, 
+//       req.body.time_of_visit, 
+//       req.body.comments)
       
-  res.sendFile(__dirname + '/ajax.html')
+//   res.sendFile(__dirname + '/ajax.html')
   
+// });
+
+app.post('/single-page-app', (req, res) => {
+    let {visitor_name,assistant,visitors_age, date_of_visit, time_of_visit, comments} = req.body;
+    console.log(visitor_name,assistant,visitors_age, date_of_visit, time_of_visit, comments)
+
+    res.sendFile(__dirname + '/ajax.html');
+
+    // res.status(200).send({
+    //     message: "Visitor added"
+    // })
 });
 
 // viewVisitor
 app.get('/view', (req, res) => {
-    return viewVisitors();
+    res.status(200).json({
+        visitor_name: "Muntu",
+        assistant: "Juju",
+        date_of_visit: "17/09/2020",
+        time_of_visit: "00:00",
+        comments: "testing"
+    });
 })
 
-// delete visitor
-app.delete('/deleteVisitor:id', (req, res) => {
-    let id = req.params.id
+// // delete visitor
+// app.delete('/deleteVisitor:id', (req, res) => {
+//     let id = req.params.id
 
-    let visitor = await deleteVisitor(id);
+//     let visitor = await deleteVisitor(id);
 
-    res.send(visitor);
+//     res.send(visitor);
 
-})
+// })
 
 // port
 const server = app.listen(3009, (req, res) => {
